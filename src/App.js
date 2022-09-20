b88ca0b934107a5c32fd5cf29c3fd49f5861249b
vikas <vikas.painuli@witarist.com>
2022-09-20 23:57:26 +0530

    02-defining-and-using-routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import './App.css';
+import { Route } from 'react-router-dom';
+
+import Welcome from './pages/Welcome';
+import Products from './pages/Products';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Let's Get Started</h1>
+    <div>
+
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
 
 export default App;
 
-// our-domain.com/ => Component A
+// our-domain.com/welcome => Component A
 // our-domain.com/products => Component B
--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+// this will activate react router
+/* <BrowserRouter>
+<App />
+</BrowserRouter> */
